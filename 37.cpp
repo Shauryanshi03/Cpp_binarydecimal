b5ef536892309b02de50409f7e1d294113a05ac4
Shauryanshi <shauryanshitripathi2005@gmail.com>
2026-04-27 17:40:10 +0530

    Add files via upload

---
+++ 37.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+     
+    int n;
+    cin>>n;
+    
+    int ans=0;
+    
+    while(n!=0){
+
+        int digit =n%10;
+        if((ans>INT_MAX/10)||(ans<INT_MIN/10)){
+         cout<<'0'<<endl;
+         break;
+        }
+        else{
+            
+            ans=(ans*10)+digit;
+            n=n/10;
+        }
+      
+        }
+          cout<<ans<<endl;
+    }
